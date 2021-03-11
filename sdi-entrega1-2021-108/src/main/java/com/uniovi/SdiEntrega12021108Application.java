009421647f36c229b4ee516df96f1a7212b42788
UO271548 <uo271548@uniovi.es>
2021-03-11 16:20:34 +0100

    SDI-2021-108-Creación del proyecto.

---
+++ sdi-entrega1-2021-108/src/main/java/com/uniovi/SdiEntrega12021108Application.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdiEntrega12021108Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdiEntrega12021108Application.class, args);
+	}
+
+}
---
+++ sdi-entrega1-2021-108/src/test/java/com/uniovi/SdiEntrega12021108ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdiEntrega12021108ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
